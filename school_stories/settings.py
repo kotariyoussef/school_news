661b8c9f4253b4ac76b17f209c126ff2abd6c234
Your Name <you@example.com>
2025-05-09 16:31:18 +0200

    whitenoise

--- school_stories/settings.py
+++ school_stories/settings.py
@@ -161,6 +161,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'    # Where collectstatic puts everything
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Use WhiteNoise's compressed and hashed storage for static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
